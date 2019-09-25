c7c94685a514c4cbf05490157a8a49a9881b328c
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-09-25 21:36:09 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118053/latihan7/integer/PBOIF210118053Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118053.latihan7.integer;
+
+/**
+ *
+ * @author
+ * NAMA : Kevin Alvian
+ * KELAS : IF2
+ * NIM : 10118053
+ */
+public class PBOIF210118053Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 900000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int: " + i);
+        System.out.println("long : " + l);
+
+    }
+    
+}
